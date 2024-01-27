5a2467d7b6e75131c2793413c7d3b36156046c18
Martin Lowe <martinlowe673@gmail.com>
2024-01-27 07:54:55 +0000

    image on

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Box,Stack,Typography} from '@mui/material'
+import {Box,Stack,Typography,Button} from '@mui/material'
+import HeroBannerImage from "../assets/images/banner.png"
 
 const HeroBanner = () => {
   return (
@@ -9,6 +10,14 @@ const HeroBanner = () => {
         <Typography color="#FF2625" fontWeight="600" fontSize="26px">
             Fitness Club
         </Typography>
+        <Typography fontWeight={700} sx={{fontsize:{lg:"44px", xs:"40px"}}}>
+            sweat smile <br /> and repeat
+        </Typography>
+        <Typography fontSize="22px" lineheight="35px" mb={3}>
+            check out best exercises
+        </Typography>
+        <Button varaint="contain" href="#exercises"color="error">Explore Exercises</Button>
+        <img src="../assets/images/banner.png" alt="banner"  className="hero-banner-img" />
     </Box>
   )
 }
